09ec984ccfdb34befd0e89db119dcc9aebcd1e47
Marcos Lourinho <marcoslourinho@protonmail.com>
2018-12-03 16:44:03 -0300

    fix error

--- android/app/src/main/java/com/firebasecam/MainApplication.java
+++ android/app/src/main/java/com/firebasecam/MainApplication.java
@@ -12,7 +12,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
-import java.util.Arrays;j
+import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
