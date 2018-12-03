952bb5c40bc46247a13d168a9080d833e2d450d0
Marcos Lourinho <marcoslourinho@protonmail.com>
2018-12-03 16:35:43 -0300

    import configs firebase storage

--- android/app/src/main/java/com/firebasecam/MainApplication.java
+++ android/app/src/main/java/com/firebasecam/MainApplication.java
@@ -6,13 +6,13 @@ import com.facebook.react.ReactApplication;
 import com.oblador.vectoricons.VectorIconsPackage;
 import io.invertase.firebase.RNFirebasePackage;
 import org.reactnative.camera.RNCameraPackage;
-import io.invertase.firebase.storage.RNFirebaseStoragePackage; // <-- storage
+import io.invertase.firebase.storage.RNFirebaseStoragePackage; // incluir referencia do FirebaseStorage
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
-import java.util.Arrays;
+import java.util.Arrays;j
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
@@ -30,7 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
             new VectorIconsPackage(),
             new RNFirebasePackage(),
             new RNCameraPackage(),
-            new RNFirebaseStoragePackage() // <-- Add this line
+            new RNFirebaseStoragePackage() // incluir new FirebaseStorage
       );
     }
 
